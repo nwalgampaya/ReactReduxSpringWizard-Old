b42a2ad8c71bf7ca854495ae9542ec62ab226570
nwalgampaya <nalinw@gmail.com>
2018-08-30 22:38:40 +1000

    Adding the changes to save the wizard

--- src/App.js
+++ src/App.js
@@ -11,100 +11,180 @@ import './index.css';
 
 
 class App extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onYearChange = this.onYearChange.bind(this);
+    this.onSexChange = this.onSexChange.bind(this);
+    // this.onPublishedChange = this.onPublishedChange.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       name: props.person ? props.person.name : "",
       year: props.person ? props.person.year : "",
       sex: props.person ? props.person.sex : "",
 
-  
+
     };
   }
-  
-// connect()
 
-
-
-render() {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-  
-  const onSubmit = async values => {
-    await sleep(300)
-    window.alert(JSON.stringify(values, 0, 2))
+  onNameChange(e) {
+    const name = e.target.value;
+    this.setState(() => ({ name: name }));
   }
+
+  onYearChange(e) {
+    const year = e.target.value;
+    this.setState(() => ({ year: year }));
+  }
+
+  onSexChange(e) {
+    const sex = e.target.value;
+    this.setState(() => ({ sex: sex }));
+  }
+
+  // connect()
+  /*
+  Old Form
+  <div >
+              <label>Name</label>
+              <Field
+                name="name"
+                component="input"
+                type="text"
+                placeholder="Jhon"
+                validate={required}
+                value={this.state.name}
+                // onChange={this.onNameChange}
   
-  const Error = ({ name }) => (
-    <Field
-      name={name}
-      subscribe={{ touched: true, error: true }}
-      render={({ meta: { touched, error } }) =>
-        touched && error ? <span>{error}</span> : null
+              />
+              <Error name="name" />
+            </div>
+            <div>
+              <label>Gender</label>
+              <Field
+                name="sex"
+                component="input"
+                type="text"
+                placeholder="Male"
+                validate={required}
+                value={this.state.sex}
+                // onChange={this.onSexChange}
+              />
+              <Error name="year" />
+            </div>
+            <div>
+              <label>Year</label>
+              <Field
+                name="year"
+                component="input"
+                type="text"
+                placeholder="1999"
+                validate={required}
+                value={this.state.year}
+                // onChange={this.onYearChange}
+              />
+              <Error name="year" />
+            </div>
+  */
+
+
+  render() {
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+    const onSubmit = async values => {
+      // e.preventDefault();
+
+      await sleep(300)
+
+      // this.state.name = "rrrr";
+      // this.state.year = "4444";
+      // this.state.sex = "No";
+      if (!this.state.name || !this.state.year || !this.state.sex) {
+        console.log("In submit If" + this.state.name);
+        this.setState(() => ({
+          error: "Please set name & year & sex!"
+        }));
+      } else {
+        console.log("In submit else");
+        this.setState(() => ({ error: "" }));
+        this.props.onSubmitPerson({
+          name: this.state.name,
+          year: this.state.year,
+          sex: this.state.sex
+          // published: this.state.published
+        });
+        window.alert(JSON.stringify(values, 0, 2))
       }
-    />
-  )
-  
-  // const required = value => (value ? undefined : 'Required')
-  
-  function required(value){
-    return value? undefined :'Required'
-  }
-  
-  return (
-    <Wizard
-    initialValues={{ employed: true, stooge: 'larry' }}
-    onSubmit={onSubmit}
-  >
-   <Wizard.Page >
-      <div >
-        <p>The CRISP-Q study.</p>
-        <p>Researchers: Yena (Grace) Kim (Scholarly Selective Student), Prof Jon Emery, A/Prof Marie Pirotta and Dr Jennifer Walker The Department of General Practice, University of Melbourne</p>
+    }
 
-        <p>Thank you for taking part in this study. We are interested in how people think about their risk of bowel cancer and their use of bowel cancer screening tests</p>          
-        <p>Who can participate? </p>
-        <p>Any person 40 years or older but younger than 75, attending a GP appointment at Deepdene Surgery can participate in the study.</p>
-        <p>What are the risks?</p>
-        <p>This survey is completely anonymous and therefore confidential, so there is no risk that we will know who said what. This study is completely voluntary and to withdraw during the study simply stop answering the questions. Due to the anonymous nature of the study we will not be able to delete your data if you withdraw.</p>
-        <p>If you are concerned about your risk of bowel cancer, please discuss this with your doctor today.</p>
-      </div>
-    </Wizard.Page>
-    <Wizard.Page >
-      <div >
-        <label>Name</label>
-        <Field
-          name="name"
-          component="input"
-          type="text"
-          placeholder="Jhon"
-          validate={required}
-        />
-        <Error name="name" />
-      </div>
-      <div>
-        <label>Gender</label>
-        <Field
-          name="sex"
-          component="input"
-          type="text"
-          placeholder="Male"
-          validate={required}
-        />
-        <Error name="year" />
-      </div>
-      <div>
-        <label>Year</label>
-        <Field
-          name="year"
-          component="input"
-          type="text"
-          placeholder="1999"
-          validate={required}
-        />
-        <Error name="year" />
-      </div>
-    </Wizard.Page>
-    <Wizard.Page
+    const Error = ({ name }) => (
+      <Field
+        name={name}
+        subscribe={{ touched: true, error: true }}
+        render={({ meta: { touched, error } }) =>
+          touched && error ? <span>{error}</span> : null
+        }
+      />
+    )
+
+    // const required = value => (value ? undefined : 'Required')
+
+    function required(value) {
+      return value ? undefined : 'Required'
+    }
+
+    return (
+      <Wizard
+        initialValues={{ employed: true, stooge: 'larry' }}
+        onSubmit={onSubmit}
+      >
+        <Wizard.Page >
+          <div >
+            <p>The CRISP-Q study.</p>
+            <p>Researchers: Yena (Grace) Kim (Scholarly Selective Student), Prof Jon Emery, A/Prof Marie Pirotta and Dr Jennifer Walker The Department of General Practice, University of Melbourne</p>
+
+            <p>Thank you for taking part in this study. We are interested in how people think about their risk of bowel cancer and their use of bowel cancer screening tests</p>
+            <p>Who can participate? </p>
+            <p>Any person 40 years or older but younger than 75, attending a GP appointment at Deepdene Surgery can participate in the study.</p>
+            <p>What are the risks?</p>
+            <p>This survey is completely anonymous and therefore confidential, so there is no risk that we will know who said what. This study is completely voluntary and to withdraw during the study simply stop answering the questions. Due to the anonymous nature of the study we will not be able to delete your data if you withdraw.</p>
+            <p>If you are concerned about your risk of bowel cancer, please discuss this with your doctor today.</p>
+          </div>
+        </Wizard.Page>
+        <Wizard.Page >
+          <div >
+            <label>Name</label>
+            <input
+              type="text"
+              placeholder="name"
+              autoFocus
+              value={this.state.name}
+              onChange={this.onNameChange}
+            />
+            <Error name="name" />
+          </div>
+          <div>
+            <label>Gender</label>
+            <input
+              type="text"
+              placeholder="year"
+              value={this.state.year}
+              onChange={this.onYearChange}
+            />
+            <Error name="year" />
+          </div>
+          <div>
+
+            <textarea
+              placeholder="sex"
+              value={this.state.sex}
+              onChange={this.onSexChange}
+            />
+            <Error name="year" />
+          </div>
+        </Wizard.Page>
+        {/* <Wizard.Page
       validate={values => {
         const errors = {}
         if (!values.email) {
@@ -205,9 +285,9 @@ render() {
         <Field name="notes" component="textarea" placeholder="Notes" />
         <Error name="notes" />
       </div>
-    </Wizard.Page>
-  </Wizard>
-  );
-}
+    </Wizard.Page> */}
+      </Wizard>
+    );
+  }
 }
 export default (App);
